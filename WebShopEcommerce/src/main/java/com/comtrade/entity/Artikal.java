ae09a139945f157cc4ac3befd7a31552e5b93411
Milos <cojcicmilos@gmail.com>
2019-07-01 18:36:10 +0200

    Dodavanje

---
+++ WebShopEcommerce/src/main/java/com/comtrade/entity/Artikal.java
@@ -0,0 +1,50 @@
+package com.comtrade.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Artikal {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String naziv;
+	private int cena;
+	private String barkod;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_kategorije")
+	
+	private Kategorije kategorije;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public int getCena() {
+		return cena;
+	}
+	public void setCena(int cena) {
+		this.cena = cena;
+	}
+	public String getBarkod() {
+		return barkod;
+	}
+	public void setBarkod(String barkod) {
+		this.barkod = barkod;
+	}
+	
+	
+	
+}
---
+++ WebShopEcommerce/src/main/java/com/comtrade/entity/Kategorije.java
@@ -0,0 +1,40 @@
+package com.comtrade.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Kategorije {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String naziv;
+	@OneToMany(mappedBy = "kategorije", cascade = CascadeType.ALL, orphanRemoval = true)
+	private Set<Artikal>setArtikala=new HashSet<Artikal>();
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public Set<Artikal> getSetArtikala() {
+		return setArtikala;
+	}
+	public void setSetArtikala(Set<Artikal> setArtikala) {
+		this.setArtikala = setArtikala;
+	}
+	
+}
